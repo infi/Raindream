b8dfe2164733b3e4bcf29592be7800377e01b3a7
Infi <sp46alt@yahoo.com>
2019-10-03 16:24:18 +0200

    Modify .gitignore

---
+++ src/Modules/PingModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Raindream;
+
+namespace Raindream.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task RunPing()
+        {
+            var msg = await Context.Channel.SendMessageAsync("⏱");
+            await msg.ModifyAsync((m_) => {
+                m_.Content = $"<:IconThis:553869005820002324> Latency: {(Context.Message.CreatedAt - msg.CreatedAt).Milliseconds}ms. API Latency: {Context.Client.Latency}ms.";
+            });
+        }
+    }
+}
